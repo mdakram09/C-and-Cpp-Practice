8da5eae64d56e32c9c5a8dc8c517fcad7012b0fc
mdakram09 <iamakramkhan09@gmail.com>
2021-03-07 11:44:12 +0530

    practiced c++

---
+++ Coding Topics/Basic/EvenOddTillN.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,N;
+	
+	cout<<"Enter the last number \n";
+	cin>>N;
+
+	for(i=0; i<=N; i++){
+
+	if(i%2!=0)
+		cout<<i<<endl;
+	}
+	
+}
