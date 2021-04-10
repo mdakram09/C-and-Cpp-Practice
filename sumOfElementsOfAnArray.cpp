e031ec1d51ce74991ab05ea066d40e86a5eb3de8
mdakram09 <iamakramkhan09@gmail.com>
2021-04-10 15:20:08 +0530

    i practiced c++ today

---
+++ sumOfElementsOfAnArray.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int size,sum=0;
+	int A[size];
+
+	cout<<"Please enter the size of the array \n";
+	cin>>size;
+
+	cout<<"Please enter the elements of the array one by one\n";
+	cout<<"                                            \n";
+	cout<<"--------------------------------------------\n";
+	cout<<"                                            \n";
+
+	for(int i=0; i<size; i++)
+	{
+		cout<<"Please enter A["<<i<<"] : "<<endl;
+		cin>>A[i];
+	}
+
+
+	cout<<"The sum of elemets in the array is \n";
+	cout<<"                                            \n";
+	cout<<"--------------------------------------------\n";
+	cout<<"                                            \n";
+
+	for(int i=0; i<size; i++)
+	{
+		sum+=A[i];
+		
+	}
+	cout<<sum<<endl<<endl;
+
+
+	return 0;
+}
