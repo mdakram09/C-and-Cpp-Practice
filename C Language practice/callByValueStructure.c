b757aa3ac2322b182ab32dd8d2a3c6ff73b134ca
mdakram09 <iamakramkhan09@gmail.com>
2021-06-01 15:06:51 +0530

    practiced structure and pointer

---
+++ C Language practice/callByValueStructure.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+struct Rectangle
+{
+	int length;
+	int breadth;
+};
+
+int area(struct Rectangle r1)
+{
+	return r1.length*r1.breadth;
+}
+
+int main()
+{
+	struct Rectangle r;
+	
+	printf("Please enter the length of the rectangle \n");
+	scanf("%d", &r.length);
+
+	printf("Please enter the breadth of the rectangle\n");
+	scanf("%d", &r.breadth);
+	
+	printf("The area of the r1 reactangle is %d\n", area(r));
+}
